670fb03185879e638fd49597eb418bd1d37ecf9d
Yeon Woo Park <ywoo1234@gmail.com>
2021-02-27 14:28:48 -0500

    Simple implementation of Internationalization. Display 'Good Morning' and 'Bonjour' depending on Accept-Language header

--- src/main/java/com/ywparkdev/rest/webservices/Application.java
+++ src/main/java/com/ywparkdev/rest/webservices/Application.java
@@ -2,6 +2,13 @@ package com.ywparkdev.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +16,21 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+//		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	// if you add in application.properties, you don't need it
+//	@Bean
+//	public ResourceBundleMessageSource bundleMessageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasename("messages");
+//		return messageSource;
+//	}
 
 }
--- src/main/java/com/ywparkdev/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/ywparkdev/rest/webservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.ywparkdev.rest.webservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello_world")
     public String helloWorld() {
@@ -20,5 +29,18 @@ public class HelloWorldController {
     @GetMapping(path = "/hello_world/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    // Option 1
+//    @GetMapping(path="/hello-world-internationalized")
+//    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+//        return messageSource.getMessage("good.morning.message", null, locale);
+//    }
+
+    // Option 2
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null,
+                LocaleContextHolder.getLocale());
     }
 }
